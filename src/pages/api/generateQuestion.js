d9e4ba8f804a798edf48e9cad3807f43451ff0dd
rogasper <royangagas@gmail.com>
2023-04-03 21:21:24 +0700

    update env

--- src/pages/api/generateQuestion.js
+++ src/pages/api/generateQuestion.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
       break;
     case "GET":
       res.status(200).json({
-        message: "ini generate quiz",
+        message: "update environment",
       });
       break;
     default:
